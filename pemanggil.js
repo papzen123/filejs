61673943f9c8011931cd72abfb4b628c23b30d53
papzen123 <49385449+papzen123@users.noreply.github.com>
2019-04-08 12:51:59 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="https://papasafelink.blogspot.com/2019/04/How-to-Avoid-a-Messy-B
 if(x=="2"){xxx="https://papasafelink.blogspot.com/2019/04/James-Ruppert-diesel-could-be-the-commuters-fuel-of-choice.html"}
 if(x=="3"){xxx="https://papasafelink.blogspot.com/2019/04/auto-insurance-quotes-how-they-are-determined.html"}
 if(x=="4"){xxx="https://papasafelink.blogspot.com/2019/04/one-piece-world-seeker-highlights-major-problem-with-anime-games.html"}
-if(x=="5"){xxx="https://papasafelink.blogspot.com/2019/04/paypal-alternatives-for-accepting-online-payments.html"}a_to_ve[i].href="https://tipsku05.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="https://papasafelink.blogspot.com/2019/04/paypal-alternatives-for-accepting-online-payments.html"}a_to_ve[i].href="https://papasafelink.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
